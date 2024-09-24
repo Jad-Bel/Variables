88088aa1f5f4b2b0fe78343f3a679890d2c0b9ed
Jad belassiria <jadthegamer06@gmail.com>
2024-09-24 18:22:14 +0100

    Variables excercices

---
+++ Variables/EX12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main() {
+    int nombre;
+
+    printf("Saisir un nombre de 2 chiffres: ");
+    scanf("%d", &nombre);
+
+    if(nombre < 1000 || nombre > 9999) {
+        printf("Entrer un nombre de 4 chiffres");
+        return 1;
+    }
+
+    int premier = nombre / 1000;
+    int deuxieme = (nombre / 100) % 10;
+    int troisieme = (nombre / 10) % 10;
+    int quatrieme = nombre % 10;
+
+    printf("L'inverse de cette nombre est: %d%d%d%d", quatrieme, troisieme, deuxieme, premier);
+    
+    return 0;
+}
